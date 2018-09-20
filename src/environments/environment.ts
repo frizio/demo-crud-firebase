28726c837d00ae78a029660f72436091f0bd44f1
Maurizio La Rocca <mrz.larocca@gmail.com>
2018-09-20 13:45:20 +0200

    Import Firebase project configuration

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyBRKgGi3n2-R3VD9JIqey7y4_zxi49EuwU",
+    authDomain: "fir-crud-angular.firebaseapp.com",
+    databaseURL: "https://fir-crud-angular.firebaseio.com",
+    projectId: "fir-crud-angular",
+    storageBucket: "fir-crud-angular.appspot.com",
+    messagingSenderId: "603456383717"
+  }
 };
 
 /*
