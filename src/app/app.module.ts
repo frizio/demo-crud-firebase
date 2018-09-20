24b8f3da661553a15ff7f31559fbd0561af39f2e
Maurizio La Rocca <mrz.larocca@gmail.com>
2018-09-20 14:30:13 +0200

    ng g c components/products/product

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,12 +8,14 @@ import { AngularFireDatabaseModule } from 'angularfire2/database';
 import { environment } from '../environments/environment';
 import { ProductsComponent } from './components/products/products.component';
 import { ProductListComponent } from './components/products/product-list/product-list.component';
+import { ProductComponent } from './components/products/product/product.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductsComponent,
-    ProductListComponent
+    ProductListComponent,
+    ProductComponent
   ],
   imports: [
     BrowserModule,
